f29bfffcced111f9a7470ad874335104425baff9
Nithinreddy1019 <kethireddynithinreddy@gmail.com>
2025-05-15 12:58:07 +0530

    cardhover: transitions

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@
 export default function Home() {
   return (
     <div className="h-screen w-full flex items-center justify-center">
-      <div className="px-12 py-4 border-black/50 rounded-[24px] border-1 hover:scale-105 hover:translate-x-2 hover:-translate-y-1 transition-all duration-100">
+      <div className="px-12 py-4 border-black/50 rounded-[24px] border-1 hover:scale-105 hover:translate-x-2 hover:-translate-y-1 transition-transform duration-100">
         We are working fine
       </div>
     </div>
