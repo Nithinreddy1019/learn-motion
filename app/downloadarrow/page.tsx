a4d62723a36d96930d9a447080550413c2c478ea
Nithinreddy1019 <kethireddynithinreddy@gmail.com>
2025-05-16 13:46:17 +0530

    downloadarrow : transition

---
+++ app/downloadarrow/page.tsx
@@ -0,0 +1,40 @@
+const Page = () => {
+    return (
+        <div className="h-screen w-full flex items-center justify-center">
+            <DownloadArrow />
+        </div>
+    );
+}
+ 
+export default Page;
+
+
+
+import "./styles.css";
+
+export function DownloadArrow() {
+  return (
+    <button aria-label="Download PDF" className="download-button">
+        {ArrowDown}
+        {ArrowDown}
+    </button>
+  );
+}
+
+const ArrowDown = (
+  <svg
+    width="18"
+    height="18"
+    viewBox="0 0 24 24"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path
+      d="M18.25 14L12 20.25L5.75 14M12 19.5V3.75"
+      stroke="black"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
+  </svg>
+);
